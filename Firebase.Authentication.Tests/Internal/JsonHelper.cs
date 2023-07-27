7dc55ba8a9c19acff0bea1ac14a2bff3dcef870c
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-07-27 12:52:14 +0200

    Added JsonHelper unit tests

---
+++ Firebase.Authentication.Tests/Internal/JsonHelper.cs
@@ -0,0 +1,34 @@
+﻿using Firebase.Authentication.Models;
+using Firebase.Authentication.Types;
+
+namespace Firebase.Authentication.Tests.Internal;
+
+internal class JsonHelper
+{
+    [Test]
+    public void Enum_Serialize()
+    {
+        // Mock model
+        FederatedUserIdentifier model = new(Provider.Google, "some-id");
+
+        // Serialize
+        string json = Authentication.Internal.Json.JsonHelper.Serialize(model);
+
+        // Write result
+        TestData.Write(json);
+    }
+
+
+    [Test]
+    public void Enum_Deserialize()
+    {
+        // Mock json
+        string json = "{\"providerId\":\"google.com\",\"rawId\":\"some-id\"}";
+
+        // Deserialize
+        FederatedUserIdentifier model = Authentication.Internal.Json.JsonHelper.Deserialize<FederatedUserIdentifier>(json);
+
+        // Write result
+        TestData.Write(model);
+    }
+}
