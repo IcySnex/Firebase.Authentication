8ac17233a3612601462055e6119b9490ae732a5b
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-08-28 16:43:26 +0200

    Implemented anonym sign in for WPF sample

---
+++ Samples/Firebase.Authentication.Sample.WPF/ViewModels/AnonymouslyViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Firebase.Authentication.Requests;
+using Microsoft.Extensions.Logging;
+
+namespace Firebase.Authentication.Sample.WPF.ViewModels;
+
+public partial class AnonymouslyViewModel : ObservableObject
+{
+    readonly HomeViewModel homeViewModel;
+
+    public AnonymouslyViewModel(
+        ILogger<AnonymouslyViewModel> logger,
+        HomeViewModel homeViewModel)
+    {
+        this.homeViewModel = homeViewModel;
+
+        logger.LogInformation("[AnonymouslyViewModel-.ctor] AnonymouslyViewModel has been initialized.");
+    }
+
+
+    [RelayCommand]
+    void NavigateBack() =>
+        homeViewModel.Navigate<ProviderViewModel>();
+
+
+    [ObservableProperty]
+    string? displayName;
+
+    [RelayCommand]
+    async Task ContinueAsync()
+    {
+        await homeViewModel.SignUpAsync(SignUpRequest.Anonymously(DisplayName));
+
+        homeViewModel.Navigate<ProviderViewModel>();
+    }
+}
---
+++ Samples/Firebase.Authentication.Sample.WPF/Views/AnonymouslyView.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Firebase.Authentication.Sample.WPF.Views;
+
+public partial class AnonymouslyView : UserControl
+{
+    public AnonymouslyView() =>
+        InitializeComponent();
+}
