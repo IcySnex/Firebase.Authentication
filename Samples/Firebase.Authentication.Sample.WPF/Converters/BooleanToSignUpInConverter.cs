b9201844d9903d3a58de343893215bda8d94cb82
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-08-30 18:32:49 +0200

    Fixed Sign Up button content when registering with email & password

---
+++ Samples/Firebase.Authentication.Sample.WPF/Converters/BooleanToSignUpInConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Firebase.Authentication.Sample.WPF.Converters;
+
+public class BooleanToSignUpInConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value ? "Sign up" : "Sign in";
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (string)value == "Sign up";
+    }
+}
