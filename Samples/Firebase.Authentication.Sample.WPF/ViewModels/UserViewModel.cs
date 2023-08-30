e5e0e5044ef760ec644f375a6a123db380009bd5
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-08-30 18:31:32 +0200

    Added ability to verify and change email address

--- Samples/Firebase.Authentication.Sample.WPF/ViewModels/UserViewModel.cs
+++ Samples/Firebase.Authentication.Sample.WPF/ViewModels/UserViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using Firebase.Authentication.Client.Interfaces;
 using Firebase.Authentication.Models;
+using Firebase.Authentication.Requests;
 using Firebase.Authentication.Sample.WPF.Services;
 using Firebase.Authentication.WPF.UI;
 using Microsoft.Extensions.Logging;
@@ -42,7 +43,9 @@ public partial class UserViewModel : ObservableObject
             case "CurrentUser":
                 PropertyChangedEventArgs<UserInfo> e = (PropertyChangedEventArgs<UserInfo>)args;
 
-                DisplayName = e.NewValue?.DisplayName ?? "";
+                DisplayName = e.NewValue?.DisplayName ?? null;
+                Email = e.NewValue?.Email ?? null;
+                IsVerifyEmailVisible = !e.NewValue?.IsEmailVerified ?? true && !string.IsNullOrWhiteSpace(e.NewValue?.Email);
                 break;
         }
     }
@@ -97,15 +100,17 @@ public partial class UserViewModel : ObservableObject
 
     async partial void OnIsEditDisplayNameChanged(bool value)
     {
+        UserInfo user = await Authenticaion.GetFreshUserAsync(TimeSpan.FromHours(1));
         try
         {
-            if (value || (await Authenticaion.GetFreshUserAsync()).DisplayName == DisplayName)
+            if (value || user.DisplayName == DisplayName)
                 return;
 
             await Authenticaion.UpdateAsync(DisplayName, null, deleteDisplayName: DisplayName is null);
         }
         catch (Exception ex)
         {
+            DisplayName = user.DisplayName;
             logger.LogInformation("[UserViewModel-.OnIsEditDisplayNameChanged] Editing display name failed: {0}", ex.Message);
             MessageBox.Show("The attempt to edit the display name was unsuccessful.\n" + ex.Message, "Editing display name failed!", MessageBoxButton.OK, MessageBoxImage.Error);
         }
@@ -117,7 +122,89 @@ public partial class UserViewModel : ObservableObject
         get => displayName;
         set => SetProperty(ref displayName, string.IsNullOrWhiteSpace(value) ? null : value);
     }
+    
 
+    [ObservableProperty]
+    bool isEditEmail = false;
+
+    async partial void OnIsEditEmailChanged(bool value)
+    {
+        UserInfo user = await Authenticaion.GetFreshUserAsync(TimeSpan.FromHours(1)); ;
+        try
+        {
+            if (value || user.Email == Email)
+                return;
+
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                MessageBox.Show("The attempt to change the email was unsuccessful.\nThe email field can not be empty.", "Editing email failed!", MessageBoxButton.OK, MessageBoxImage.Error);
+                Email = user.Email;
+                return;
+            }
+
+            if (MessageBox.Show("If you continue you will get a change email sent to your account. Once verified you will be able to use your new email. If you press 'Yes' you will get signed out!\nDo you want to continue?", "Are you sure?", MessageBoxButton.YesNo, MessageBoxImage.Warning) != MessageBoxResult.Yes)
+            {
+                Email = user.Email;
+                return;
+            }
+
+            await Authenticaion.SendEmailAsync(EmailRequest.Change(Email));
+            SignOut();
+        }
+        catch (Exception ex)
+        {
+            Email = user.Email;
+            logger.LogInformation("[UserViewModel-.OnIsEditEmailChanged] Editing email failed: {0}", ex.Message);
+            MessageBox.Show("The attempt to change the email was unsuccessful.\n" + ex.Message, "Editing email name failed!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private string? email;
+    public string? Email
+    {
+        get => email;
+        set => SetProperty(ref email, string.IsNullOrWhiteSpace(value) ? null : value);
+    }
+
+
+    [ObservableProperty]
+    bool isVerifyEmailVisible = true;
+
+    [RelayCommand]
+    void CloseVerifyEmail() =>
+        IsVerifyEmailVisible = false;
+
+    [RelayCommand]
+    async Task VerifyEmailAsync()
+    {
+        try
+        {
+            await Authenticaion.SendEmailAsync(EmailRequest.Verify(), "en");
+            IsVerifyEmailVisible = false;
+            MessageBox.Show("A verify email was successfully sent to your account. Please check your email inbox!", "Verify email sent!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        catch (Exception ex)
+        {
+            logger.LogInformation("[UserViewModel-.VerifyEmailAsync] Verifying email failed: {0}", ex.Message);
+            MessageBox.Show("The attempt to send an verify email was unsuccessful.\n" + ex.Message, "Verifying email failed!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+
+    [RelayCommand]
+    async Task RefreshAsync()
+    {
+        try
+        {
+            await Authenticaion.GetFreshUserAsync(TimeSpan.FromHours(1), true);
+            MessageBox.Show("The user info successfully refreshed!", "Refreshed user info!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        catch (Exception ex)
+        {
+            logger.LogInformation("[UserViewModel-.RefreshAsync] Refreshing user info failed: {0}", ex.Message);
+            MessageBox.Show("The attempt to refresh the user info was unsuccessful.\n" + ex.Message, "Refreshing user info failed!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
 
     [RelayCommand]
     void SignOut()
