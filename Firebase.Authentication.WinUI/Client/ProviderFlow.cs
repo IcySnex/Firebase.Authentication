157daf78821ec41c09d95b9e1328f8465ad9987e
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-09-06 12:51:02 +0200

    Added ProviderFlow base and icons

---
+++ Firebase.Authentication.WinUI/Client/ProviderFlow.cs
@@ -0,0 +1,241 @@
+﻿using Firebase.Authentication.Client.Interfaces;
+using Firebase.Authentication.Models;
+using Firebase.Authentication.Requests;
+using Firebase.Authentication.Types;
+using Firebase.Authentication.WinUI.Configuration;
+using Firebase.Authentication.WinUI.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+
+namespace Firebase.Authentication.WinUI.Client;
+
+/// <summary>
+/// Handles 3rd party OAuth provider authentication with a new window
+/// </summary>
+public abstract class ProviderFlow : IProviderFlow
+{
+    readonly Provider provider;
+    readonly (int width, int height) windowSize;
+    string redirectTo;
+
+    readonly ILogger<IProviderFlow>? logger;
+
+    /// <summary>
+    /// Creates a new ProviderFlow
+    /// </summary>
+    /// <param name="windowConfig">The configuration the provider flow window will be created with</param>
+    /// <param name="provider">The provider used for this flow</param>
+    /// <param name="windowSize">The size of the provider flow window</param>
+    /// <param name="redirectTo">The url to which the provider will redirect the user back to</param>
+    public ProviderFlow(
+        WindowConfig windowConfig,
+        Provider provider,
+        (int width, int height) windowSize,
+        string redirectTo)
+    {
+        WindowConfiguration = windowConfig;
+        this.provider = provider;
+        this.windowSize = windowSize;
+        this.redirectTo = redirectTo;
+    }
+
+    /// <summary>
+    /// Creates a new ProviderFlow with extendended logging functions
+    /// </summary>
+    /// <param name="windowConfig">The configuration the provider flow window will be created with</param>
+    /// <param name="provider">The provider used for this flow</param>
+    /// <param name="logger">The logger which will be used to log</param>
+    /// <param name="windowSize">The size of the provider flow window</param>
+    /// <param name="redirectTo">The url to which the provider will redirect the user back to</param>
+    public ProviderFlow(
+        WindowConfig windowConfig,
+        Provider provider,
+        (int width, int height) windowSize,
+        string redirectTo,
+        ILogger<IProviderFlow> logger)
+    {
+        WindowConfiguration = windowConfig;
+        this.provider = provider;
+        this.windowSize = windowSize;
+        this.redirectTo = redirectTo;
+
+        this.logger = logger;
+        logger.LogInformation("[ProviderFlow-.ctor] ProviderFlow has been initialized.");
+    }
+
+
+    /// <summary>
+    /// The window configuration used for this provider flow
+    /// </summary>
+    public WindowConfig WindowConfiguration { get; set; }
+
+
+    Window window = default!;
+    WebView2 webView = default!;
+    WindowHelper windowHelper = default!;
+
+    void CreateWindowAndWebView()
+    {
+        webView = new();
+        window = new()
+        {
+            Title = WindowConfiguration.Title,
+            Content = webView
+        };
+        windowHelper = new(window, logger);
+
+        windowHelper.SetIcon(WindowConfiguration.Icon);
+        windowHelper.SetSize(windowSize.width, windowSize.height);
+        windowHelper.SetPosition(WindowConfiguration.StartupLocation, WindowConfiguration.Left, WindowConfiguration.Top, WindowConfiguration.Owner);
+
+        logger?.LogInformation("[ProviderFlow-CreateWindowAndWebView] Created provider flow window with webView");
+    }
+
+
+    /// <summary>
+    /// Authenticates the given client with this provider flow
+    /// </summary>
+    /// <param name="authentication">The authentication client to authenticate</param>
+    /// <param name="cancellationToken">The token to cancel this action</param>
+    /// <exception cref="Firebase.Authentication.Exceptions.IdentityPlatformException">Occurs when the request failed on the Firebase Server</exception>
+    /// <exception cref="System.NotSupportedException">May occurs when the json serialization fails</exception>
+    /// <exception cref="System.InvalidOperationException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Net.Http.HttpRequestException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Threading.Tasks.TaskCanceledException">Occurs when The task was cancelled</exception>
+    async Task<(string redirectedUrl, string sessionId)> AuthenticateAsync(
+        IAuthenticationClient authentication,
+        CancellationToken cancellationToken = default)
+    {
+        logger?.LogInformation($"[ProviderFlow-SignInAsync] Provider ({provider}) flow authenticaion requested.");
+        ProviderRedirect redirect = await authentication.CreateProviderRedirectAsync(provider, redirectTo, cancellationToken);
+
+        // Define result
+        string? redirectedUrl = null;
+        TaskCompletionSource<string> taskWaiter = new();
+
+        // Create cancellation token based on timeout
+        CancellationTokenSource cancelSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        cancelSource.Token.Register(() =>
+        {
+            taskWaiter.TrySetCanceled();
+            window?.DispatcherQueue.TryEnqueue(window.Close);
+
+            logger?.LogInformation($"[ProviderFlow-OnTokenCancelled] Provider ({provider}) flow authenticaion was cancelled");
+        });
+
+
+        // Create UI
+        CreateWindowAndWebView();
+
+        window.Closed += OnWindowClosed;
+        void OnWindowClosed(object? _, object _1)
+        {
+            window.Closed -= OnWindowClosed;
+
+            // Clear cache
+            webView.CoreWebView2.Profile.ClearBrowsingDataAsync().AsTask();
+            webView.Close();
+
+            // Clear UI
+            window = default!;
+            windowHelper = default!;
+            webView = default!;
+
+            // If redirectedUrl stil not set cancel
+            if (redirectedUrl is null)
+                cancelSource.Cancel();
+            cancelSource.Dispose();
+
+            // If SetWindowLongPtr was set before, owner window will be minimized at closing => Manually activate it
+            if (WindowConfiguration.ShowAsDialog && WindowConfiguration.Owner is not null)
+                WindowConfiguration.Owner.Activate();
+
+
+            logger?.LogInformation("[ProviderFlow-OnWindowClosed] Provider flow window was closed and objects were disposed");
+        }
+
+        window.Activate();
+        if (WindowConfiguration.ShowAsDialog && WindowConfiguration.Owner is not null)
+            windowHelper.MakeDialog(WindowConfiguration.Owner);
+
+
+        // Setup WebView
+        logger?.LogInformation("[ProviderFlow-SignInAsync] Setting up WebView2");
+
+        await webView.EnsureCoreWebView2Async();
+
+        webView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+        webView.CoreWebView2.Settings.IsZoomControlEnabled = false;
+        webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
+        webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+
+        webView.CoreWebView2.NavigationStarting += NavigationStarting;
+        void NavigationStarting(object? _, CoreWebView2NavigationStartingEventArgs e)
+        {
+            // If navigation contains redirected back url set result
+            if (e.Uri.StartsWith(redirectTo))
+                taskWaiter.SetResult(e.Uri);
+        }
+
+        if (provider == Provider.Facebook)
+            webView.CoreWebView2.NavigationCompleted += async (s, e) => await webView.CoreWebView2.ExecuteScriptAsync("document.querySelector(`button[title=\"Decline optional cookies\"]`)?.click()");
+
+
+        // Authenticate
+        webView.CoreWebView2.Navigate(redirect.Uri);
+        redirectedUrl = await taskWaiter.Task;
+        window.Close();
+
+        logger?.LogInformation("[ProviderFlow-SignInAsync] Provider flow was successfully authenticated");
+        return (redirectedUrl, redirect.SessionId);
+    }
+
+
+    /// <summary>
+    /// Signs in the given client with this provider flow
+    /// </summary>
+    /// <param name="authentication">The authentication client to sign into</param>
+    /// <param name="cancellationToken">The token to cancel this action</param>
+    /// <exception cref="Firebase.Authentication.Exceptions.CredentialAlreadyExistException">Occurrs when the current credential is not null</exception>
+    /// <exception cref="Firebase.Authentication.Exceptions.IdentityPlatformException">Occurs when the request failed on the Firebase Server</exception>
+    /// <exception cref="System.NotSupportedException">May occurs when the json serialization fails</exception>
+    /// <exception cref="System.InvalidOperationException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Net.Http.HttpRequestException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Threading.Tasks.TaskCanceledException">Occurs when The task was cancelled</exception>
+    public async Task SignInAsync(
+        IAuthenticationClient authentication,
+        CancellationToken cancellationToken = default)
+    {
+        // Authenticate
+        (string redirectedUrl, string sessionId) providerInfo = await AuthenticateAsync(authentication, cancellationToken);
+
+        // Sign in
+        SignInRequest request = SignInRequest.WithProviderRedirect(providerInfo.redirectedUrl, providerInfo.sessionId);
+        await authentication.SignInAsync(request, cancellationToken);
+    }
+
+    /// <summary>
+    /// Links the current user of the given client with this provider flow
+    /// </summary>
+    /// <param name="authentication">The authentication client to sign into</param>
+    /// <param name="cancellationToken">The token to cancel this action</param>
+    /// <exception cref="Firebase.Authentication.Exceptions.CredentialAlreadyExistException">Occurrs when the current credential is not null</exception>
+    /// <exception cref="Firebase.Authentication.Exceptions.IdentityPlatformException">Occurs when the request failed on the Firebase Server</exception>
+    /// <exception cref="System.NotSupportedException">May occurs when the json serialization fails</exception>
+    /// <exception cref="System.InvalidOperationException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Net.Http.HttpRequestException">May occurs when sending the web request fails</exception>
+    /// <exception cref="System.Threading.Tasks.TaskCanceledException">Occurs when The task was cancelled</exception>
+    public async Task LinkAsync(
+        IAuthenticationClient authentication,
+        CancellationToken cancellationToken = default)
+    {
+        // Authenticate
+        (string redirectedUrl, string sessionId) providerInfo = await AuthenticateAsync(authentication, cancellationToken);
+
+        // Link
+        LinkRequest request = LinkRequest.ToProviderRedirect(providerInfo.redirectedUrl, providerInfo.sessionId);
+        await authentication.LinkAsync(request, cancellationToken);
+    }
+}
---
+++ Firebase.Authentication.WinUI/Internal/WindowHelper.cs
@@ -0,0 +1,207 @@
+﻿using Firebase.Authentication.WinUI.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Windows.Graphics;
+using WinRT.Interop;
+
+namespace Firebase.Authentication.WinUI.Internal;
+
+internal class WindowHelper
+{
+    readonly AppWindow window;
+    readonly ILogger? logger;
+
+    /// <summary>
+    /// Creates a new WindowHelper with with optional logging functions
+    /// </summary>
+    /// <param name="window">The winodw which is used in the helper</param>
+    /// <param name="logger">The logger which will be used for logging</param>
+    public WindowHelper(
+        Window window,
+        ILogger? logger = null)
+    {
+        HWnd = GetHWnd(window);
+        this.window = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(HWnd));
+        this.logger = logger;
+
+        ((OverlappedPresenter)this.window.Presenter).IsResizable = false;
+        ((OverlappedPresenter)this.window.Presenter).IsMinimizable = false;
+        ((OverlappedPresenter)this.window.Presenter).IsMaximizable = false;
+    }
+
+
+    /// <summary>
+    /// Gets the position and size of an owner window
+    /// </summary>
+    /// <param name="owner">The owner window</param>
+    /// <returns>The position and size</returns>
+    (PointInt32, SizeInt32) GetOwnerWindow(
+        Window owner)
+    {
+        AppWindow ownerWindow = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(GetHWnd(owner)));
+        return (ownerWindow.Position, ownerWindow.Size);
+    }
+
+    /// <summary>
+    /// Gets the HWND of an owner window
+    /// </summary>
+    /// <param name="owner">The owner window</param>
+    /// <returns></returns>
+    IntPtr GetHWnd(
+        Window owner) =>
+        WindowNative.GetWindowHandle(owner);
+
+
+    private static IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong) =>
+        IntPtr.Size == 8 ? SetWindowLongPtr64(hWnd, nIndex, dwNewLong) : new(SetWindowLong32(hWnd, nIndex, dwNewLong.ToInt32()));
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+    private static extern int SetWindowLong32(IntPtr hWnd, int nIndex, int dwNewLong);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+    private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+    [DllImport("user32.dll", EntryPoint = "GetDpiForWindow")]
+    private static extern int GetDpiForWindow(IntPtr hwnd);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+
+    /// <summary>
+    /// HWND of the current main window
+    /// </summary>
+    public readonly IntPtr HWnd;
+
+    /// <summary>
+    /// Position of the current main window
+    /// </summary>
+    public PointInt32 Position => window.Position;
+
+    /// <summary>
+    /// Size of the current main window
+    /// </summary>
+    public SizeInt32 Size => window.Size;
+
+    /// <summary>
+    /// Size of the current main screen
+    /// </summary>
+    public RectInt32 ScreenSize => DisplayArea.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(HWnd), DisplayAreaFallback.Nearest).WorkArea;
+
+
+    /// <summary>
+    /// Makes the current main window a dialog window
+    /// </summary>
+    /// <param name="owner">The owner to set for the current main window</param>
+    public void MakeDialog(
+        Window owner)
+    {
+        SetWindowLongPtr(HWnd, -8, GetHWnd(owner));
+        ((OverlappedPresenter)window.Presenter).IsModal = true;
+    }
+
+
+    /// <summary>
+    /// Sets a custom icon on the current main window
+    /// </summary>
+    /// <param name="icon">The bitmap icon</param>
+    public void SetIcon(
+        Bitmap icon)
+    {
+        if (icon is null)
+            return;
+
+        IntPtr hicon = icon.GetHicon();
+
+        SendMessage(HWnd, 128, (IntPtr)0, hicon);
+        SendMessage(HWnd, 128, (IntPtr)1, hicon);
+
+        logger?.LogInformation("[WindowHelper-SetIcon] Set app icon to bitmap");
+    }
+
+    /// <summary>
+    /// Sets the size of the current main window
+    /// </summary>
+    /// <param name="width">The width of the new size</param>
+    /// <param name="height">The height of the new size</param>
+    public void SetSize(
+        int width,
+        int height)
+    {
+        double scaling = GetDpiForWindow(HWnd) / 96.0;
+        window.Resize(new((int)((width + 16) * scaling), (int)((height + 39) * scaling)));
+
+        logger?.LogInformation("[WindowHelper-SetSize] Set window size [{width}x{height}]", width, height);
+    }
+
+
+    /// <summary>
+    /// Sets the position of the current main window
+    /// </summary>
+    /// <param name="x">The x coordinate of the new position</param>
+    /// <param name="y">The y coordinate of the new position</param>
+    public void SetPositionToPoint(
+        int x,
+        int y)
+    {
+        window.Move(new(x, y));
+        logger?.LogInformation("[WindowHelper-SetPosition] Set window position [{x}, {y}]", x, y);
+    }
+
+    /// <summary>
+    /// Sets the position of the current main window to the cetner of the main screen
+    /// </summary>
+    public void SetPositionToCenter() =>
+        SetPositionToPoint((ScreenSize.Width - Size.Width) / 2, (ScreenSize.Height - Size.Height) / 2);
+
+    /// <summary>
+    /// Sets the position of the current main window to the cetner of the owner window
+    /// </summary>
+    /// <param name="owner">The owner of the current main window</param>
+    public void SetPositionToOwnerCenter(
+        Window owner)
+    {
+        (PointInt32 ownerPosition, SizeInt32 ownerSize) = GetOwnerWindow(owner);
+
+        int left = ownerPosition.X + (ownerSize.Width / 2) - (Size.Width / 2);
+        left = left < 0 ? 0 : left > ScreenSize.Width - Size.Width ? ScreenSize.Width - Size.Width : left;
+        int top = ownerPosition.Y + (ownerSize.Height / 2) - (Size.Height / 2);
+        top = top < 0 ? 0 : top > ScreenSize.Height - Size.Height ? ScreenSize.Height - Size.Height - 30 : top;
+
+        SetPositionToPoint(left, top);
+    }
+
+    /// <summary>
+    /// Sets the position of the current main window
+    /// </summary>
+    /// <param name="location">The default location of the window</param>
+    /// <param name="x">The x coordinate of the new position</param>
+    /// <param name="y">The y coordinate of the new position</param>
+    /// <param name="owner">The owner of the current main window</param>
+    public void SetPosition(
+        WindowStartupLocation location,
+        int x,
+        int y,
+        Window? owner)
+    {
+
+        switch (location)
+        {
+            case WindowStartupLocation.CenterScreen:
+                SetPositionToCenter();
+                return;
+            case WindowStartupLocation.CenterOwner:
+                if (owner is null)
+                    break;
+
+                SetPositionToOwnerCenter(owner);
+                return;
+        }
+
+        SetPositionToPoint(x, y);
+    }
+}
