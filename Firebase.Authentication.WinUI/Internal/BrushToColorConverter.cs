4358d0d7d8236809d55f8313862d574af750765f
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-09-06 16:13:45 +0200

    Added FirebaseAuthenticationButton with provider styles

---
+++ Firebase.Authentication.WinUI/Internal/BrushToColorConverter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+
+namespace Firebase.Authentication.WinUI.Internal;
+
+internal class BrushToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language) =>
+        value is SolidColorBrush brush ? brush.Color : Colors.Transparent;
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+        throw new NotImplementedException();
+}
