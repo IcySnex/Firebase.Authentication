164350ff3b7c8eb9d9740f38a64428fb8b87d192
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-09-09 13:50:25 +0200

    Added xaml converters

---
+++ Samples/Firebase.Authentication.Sample.WinUI/Converters/BooleanToSignUpInConverter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace Firebase.Authentication.Sample.WinUI.Converters;
+
+public class BooleanToSignUpInConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string language) =>
+        (bool)value ? "Sign up" : "Sign in";
+
+    public object? ConvertBack(object value, Type targetType, object parameter, string language) =>
+        (string)value == "Save";
+}
---
+++ Samples/Firebase.Authentication.Sample.WinUI/Converters/TimeSpanConverter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System.Globalization;
+
+namespace Firebase.Authentication.Sample.WinUI.Converters;
+
+public class TimeSpanConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string language) =>
+        value is TimeSpan time ? time.ToString("hh':'mm':'ss") : null;
+
+    public object? ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        bool parsed = TimeSpan.TryParseExact((string)value, "hh':'mm':'ss", CultureInfo.CurrentCulture, TimeSpanStyles.None, out TimeSpan result);
+        return parsed ? result : null;
+    }
+}
