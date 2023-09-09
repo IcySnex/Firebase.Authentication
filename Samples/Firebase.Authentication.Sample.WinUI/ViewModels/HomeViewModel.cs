d5a3cfdb0ba3cae2f1d5578f54dd9ad0074c1682
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-09-09 13:50:42 +0200

    Introduced home page

---
+++ Samples/Firebase.Authentication.Sample.WinUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,102 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Firebase.Authentication.Client.Interfaces;
+using Firebase.Authentication.Requests;
+using Firebase.Authentication.Sample.WinUI.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.UI.Xaml.Controls;
+using Windows.System;
+
+namespace Firebase.Authentication.Sample.WinUI.ViewModels;
+
+public partial class HomeViewModel : ObservableObject
+{
+    readonly ILogger<HomeViewModel> logger;
+    readonly Models.Configuration configuration;
+    readonly WindowHelper windowHelper;
+    readonly Navigation navigation;
+    readonly IAuthenticationClient authentication;
+
+    public HomeViewModel(
+        ILogger<HomeViewModel> logger,
+        IOptions<Models.Configuration> configuration,
+        WindowHelper windowHelper,
+        Navigation navigation,
+        IAuthenticationClient authentication)
+    {
+        this.logger = logger;
+        this.configuration = configuration.Value;
+        this.windowHelper = windowHelper;
+        this.navigation = navigation;
+        this.authentication = authentication;
+
+        logger.LogInformation("[HomeViewModel-.ctor] HomeViewModel has been initialized.");
+    }
+
+
+    [ObservableProperty]
+    Page? currentView;
+
+    public void Navigate(
+        Page? view)
+    {
+        CurrentView = view;
+        logger.LogInformation("[HomeViewModel.Navigate] Navigated to view.");
+    }
+
+
+    [RelayCommand]
+    async Task OpenBrowserAsync(
+        string url)
+    {
+        await Launcher.LaunchUriAsync(new(url));
+        logger.LogInformation("[HomeViewModel-OpenBrowser] Opened browser with url.");
+    }
+
+
+    CancellationTokenSource? cancelSource;
+
+    public async Task<bool> SignInAsync(
+        SignInRequest request)
+    {
+        try
+        {
+            cancelSource = new(configuration.Timeout);
+            await authentication.SignInAsync(request, cancelSource.Token);
+
+            navigation.NavigateSilent("User");
+            navigation.SetNavigationViewItemVisibility(0, false);
+            navigation.SetNavigationViewItemVisibility(1, true);
+            return true;
+        }
+        catch (TaskCanceledException) { }
+        catch (Exception ex)
+        {
+            await windowHelper.AlertErrorAsync(ex, "Signing in failed", "HomeViewModel-SignUpAsync");
+        }
+        return false;
+    }
+
+    public async Task<bool> SignUpAsync(
+        SignUpRequest request)
+    {
+        try
+        {
+            cancelSource = new(configuration.Timeout);
+            await authentication.SignUpAsync(request, cancelSource.Token);
+
+            navigation.NavigateSilent("User");
+            navigation.SetNavigationViewItemVisibility(0, false);
+            navigation.SetNavigationViewItemVisibility(1, true);
+            return true;
+        }
+        catch (TaskCanceledException) { }
+        catch (Exception ex)
+        {
+            await windowHelper.AlertErrorAsync(ex, "Signing up failed", "HomeViewModel-SignUpAsync");
+        }
+        return false;
+    }
+
+}
---
+++ Samples/Firebase.Authentication.Sample.WinUI/ViewModels/UserViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Firebase.Authentication.Client.Interfaces;
+using Firebase.Authentication.Models;
+using Firebase.Authentication.Sample.WinUI.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Firebase.Authentication.Sample.WinUI.ViewModels;
+
+public class UserViewModel
+{
+    readonly ILogger<UserViewModel> logger;
+    readonly Navigation navigation;
+    readonly ImageUploader imageUploader;
+
+    public IAuthenticationClient Authentication { get; }
+
+    public UserViewModel(
+        ILogger<UserViewModel> logger,
+        Navigation navigation,
+        ImageUploader imageUploader,
+        IAuthenticationClient authentication)
+    {
+        this.logger = logger;
+        this.navigation = navigation;
+        this.imageUploader = imageUploader;
+
+        this.Authentication = authentication;
+
+        Authentication.PropertyChanged += OnAuthenticationPropertyChanged;
+        OnAuthenticationPropertyChanged(null, new PropertyChangedEventArgs<UserInfo>("CurrentUser", null, Authentication.CurrentUser));
+
+        logger.LogInformation("[UserViewModel-.ctor] UserViewModel has been initialized.");
+    }
+
+    void OnAuthenticationPropertyChanged(object? _, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        switch (e)
+        {
+            case PropertyChangedEventArgs<UserInfo> args:
+                //DisplayName = args.NewValue?.DisplayName ?? null;
+
+                //Email = args.NewValue?.Email ?? null;
+
+                //IsVerifyEmailVisible = !(args.NewValue?.IsEmailVerified ?? true) && !string.IsNullOrWhiteSpace(args.NewValue?.Email);
+
+                //UsedSignInMethods = args.NewValue?.ProviderUserInfos?.Select(info => info.Provider).ToArray() ?? null;
+                //IsAddSignInMethodVisible = UsedSignInMethods is null ? true : Enum.GetValues<Provider>().Except(UsedSignInMethods).Any();
+                break;
+        }
+    }
+}
