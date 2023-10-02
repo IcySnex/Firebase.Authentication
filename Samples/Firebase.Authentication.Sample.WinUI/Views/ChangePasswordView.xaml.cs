cb2ebf85dd6a5a1561ea7922609cfee80d5fd86e
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-10-02 10:20:51 +0200

    Introduced ability to change password

---
+++ Samples/Firebase.Authentication.Sample.WinUI/Views/ChangePasswordView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Firebase.Authentication.Sample.WinUI.Views;
+
+public sealed partial class ChangePasswordView : Page
+{
+    public ChangePasswordView()
+    {
+        InitializeComponent();
+    }
+}
