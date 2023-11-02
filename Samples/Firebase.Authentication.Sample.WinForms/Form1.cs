d447adbeef4f7eb9f493f24504420e7a320f1b49
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-11-02 12:29:50 +0100

    Reset WinForms sample

--- Samples/Firebase.Authentication.Sample.WinForms/Form1.cs
+++ Samples/Firebase.Authentication.Sample.WinForms/Form1.cs
@@ -1,7 +1,12 @@
 using Firebase.Authentication.Client;
 using Firebase.Authentication.Client.Interfaces;
 using Firebase.Authentication.Configuration;
+using Firebase.Authentication.Sample.WinForms.Properties;
 using Firebase.Authentication.WinForms.Flows;
+using Firebase.Authentication.WinForms.UI;
+using Svg;
+using System.ComponentModel;
+using System.Drawing.Drawing2D;
 
 namespace Firebase.Authentication.Sample.WinForms;
 
@@ -10,20 +15,5 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
-    }
-
-
-    private async void button1_Click(object sender, EventArgs e)
-    {
-        IProviderFlow flow = new YahooProviderFlow(new("ass", null!, this, FormStartPosition.CenterParent, 0, 0, true));
-        IAuthenticationClient client = new AuthenticationClient(new AuthenticationConfig("AIzaSyALFTcLBy2mjtgCjKfIJ82Ivu-wVR3w9Z4"));
-
-        try
-        {
-            await flow.SignInAsync(client, new CancellationTokenSource(10000).Token);
-        } catch (Exception ex)
-        {
-
-        }
     }
 }
